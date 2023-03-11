001a63090b001d9a6ef99bc3c7be664c1a481103
Emmanuel Lokossou <sewonou.lokossou1988@gmail.com>
2023-03-11 19:10:36 +0000

    Mise en place de la login page + security.yaml

--- src/Controller/Admin/AdminAccountController.php
+++ src/Controller/Admin/AdminAccountController.php
@@ -5,14 +5,24 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AdminAccountController extends AbstractController
 {
-    #[Route('/admin/account', name: 'app_admin_account')]
-    public function index(): Response
+    #[Route('/admin/login', name: 'app_admin_login')]
+    public function login(AuthenticationUtils $utils): Response
     {
-        return $this->render('admin/account/index.html.twig', [
-            'controller_name' => 'AdminAccountController',
+        $error = $utils->getLastAuthenticationError();
+
+        return $this->render('admin/account/login.html.twig', [
+            'last_username' => $utils->getLastUsername(),
+            'hasError' => $error !== null,
         ]);
+    }
+
+    #[Route('/admin/logout', name: 'app_admin_logout')]
+    public function logout(): Response
+    {
+
     }
 }
